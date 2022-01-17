e5a81571b64bf999f99750525407e01686a7ca92
ricardocolina <33906430+ricardocolina@users.noreply.github.com>
2022-01-17 21:24:13 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,38 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    while (enviado == 0) {
+        basic.pause(100)
+    }
+    basic.clearScreen()
+    basic.pause(500)
+    if (receivedNumber == 1) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # # # # #
+            . # # # .
+            . . # . .
+            `)
+    } else if (receivedNumber == 2) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+    } else {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            # # . # #
+            # # . # #
+            `)
+    }
+    basic.pause(1000)
+    enviado = 0
+    basic.clearScreen()
+})
 input.onButtonPressed(Button.A, function () {
     basic.showLeds(`
         . . # . .
@@ -6,8 +41,9 @@ input.onButtonPressed(Button.A, function () {
         . # # # .
         . . # . .
         `)
-    basic.pause(3000)
-    basic.clearScreen()
+    radio.sendNumber(1)
+    basic.pause(1000)
+    enviado = 1
 })
 input.onButtonPressed(Button.AB, function () {
     basic.showLeds(`
@@ -17,8 +53,9 @@ input.onButtonPressed(Button.AB, function () {
         # # . # #
         # # . # #
         `)
-    basic.pause(3000)
-    basic.clearScreen()
+    radio.sendNumber(3)
+    basic.pause(1000)
+    enviado = 1
 })
 input.onButtonPressed(Button.B, function () {
     basic.showLeds(`
@@ -28,9 +65,12 @@ input.onButtonPressed(Button.B, function () {
         # . . . #
         # # # # #
         `)
-    basic.pause(3000)
-    basic.clearScreen()
+    radio.sendNumber(2)
+    basic.pause(1000)
+    enviado = 1
 })
+let enviado = 0
+radio.setGroup(1)
 basic.forever(function () {
 	
 })
